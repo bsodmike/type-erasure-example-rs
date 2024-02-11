8b21b7a6379347515c0ff59496f2e89f61b2e06e
Michael de Silva <michael@mwdesilva.com>
2024-02-11 21:47:23 +0530

    Support Debug

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@
 use core::fmt::Debug;
 use std::{any::Any, fmt::Display, marker::PhantomData};
 
-// trait DisplayWithDebugTrait: Display + Debug {}
+trait DisplayWithDebugTrait: Display + Debug {}
+impl<'a> DisplayWithDebugTrait for Box<dyn 'a + DisplayWithDebugTrait> {}
+impl<'a, T: Clone + Display + Debug> DisplayWithDebugTrait for T {}
 
 trait PinService<'a, Request> {
-    type Response: Display;
+    type Response: DisplayWithDebugTrait;
 
     fn call(&mut self, req: Request) -> Self::Response;
 }
@@ -27,9 +29,9 @@ where
     S: PinService<'a, Request, Response = Response>,
     S::Response: 'a,
     Request: Any,
-    Response: 'a + ToString + Display,
+    Response: 'a + DisplayWithDebugTrait,
 {
-    type Response = Box<dyn 'a + Display>;
+    type Response = Box<dyn 'a + DisplayWithDebugTrait>;
 
     fn call(&mut self, req: Box<dyn Any>) -> Self::Response {
         let req: Request = *req.downcast::<Request>().ok().expect("wrong request type");
@@ -40,11 +42,11 @@ where
 }
 
 type BoxUntypedService<'a> =
-    Box<dyn 'a + PinService<'a, Box<dyn Any>, Response = Box<dyn 'a + Display>>>;
+    Box<dyn 'a + PinService<'a, Box<dyn Any>, Response = Box<dyn 'a + DisplayWithDebugTrait>>>;
 
 fn boxed_untyped_service<'a, S, Request>(svc: S) -> BoxUntypedService<'a>
 where
-    S: 'a + PinService<'a, Request, Response = Box<(dyn std::fmt::Display + 'a)>>,
+    S: 'a + PinService<'a, Request, Response = Box<(dyn 'a + DisplayWithDebugTrait)>>,
     S::Response: 'a,
     Request: Any,
 {
@@ -90,7 +92,7 @@ where
 struct Foo {}
 
 impl<'a> PinService<'a, i64> for Foo {
-    type Response = Box<dyn 'a + Display>;
+    type Response = Box<dyn 'a + DisplayWithDebugTrait>;
 
     fn call(&mut self, req: i64) -> Self::Response {
         Box::new(format!("got i64: {req:?}"))
@@ -103,7 +105,7 @@ struct Point(f64, f64);
 struct Bar {}
 
 impl<'a> PinService<'a, (f64, f64)> for Bar {
-    type Response = Box<dyn 'a + Display>;
+    type Response = Box<dyn 'a + DisplayWithDebugTrait>;
 
     fn call(&mut self, req: (f64, f64)) -> Self::Response {
         Box::new(format!("{:?}", Point(req.0, req.1)))
@@ -114,7 +116,7 @@ fn main() {
     let mut svc = boxed_untyped_service(Foo {});
 
     let resp = svc.call(Box::new(123i64));
-    println!("{resp}");
+    println!("{resp:?}");
 
     let resp = boxed_untyped_service(Bar {}).call(Box::new((123.0f64, 456.0f64)));
     println!("{resp}");
